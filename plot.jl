4481ce4bd3f907ff035f1c37d1b12c61ad3c990f
tortar <adrianomeligrana1999@gmail.com>
2025-05-30 00:21:08 +0200

    improve style

--- plot.jl
+++ plot.jl
@@ -7,11 +7,11 @@ ts_static = CSV.read("data/static.csv", DataFrame)
 ts_dynamic_fixed_dom = CSV.read("data/dynamic_fixed.csv", DataFrame)
 ts_dynamic_var_dom = CSV.read("data/dynamic_variable.csv", DataFrame)
 
-ts_static = select(ts_static, [:EBUS, :FOREST_OF_TREES, :PROPOSAL_ARRAY])
+ts_static = select(ts_static, [:EBUS, :BUS, :FOREST_OF_TREES, :PROPOSAL_ARRAY])
 rename_map = Dict(:EBUS => :EBUS, :FOREST_OF_TREES => :FT, :PROPOSAL_ARRAY => :DPA, :BUS => :BUS)
 rename!(ts_static, rename_map)
 
-ts_dynamic_fixed_dom = select(ts_dynamic_fixed_dom, [:EBUS, :FOREST_OF_TREES, :PROPOSAL_ARRAY, :BUS])
+ts_dynamic_fixed_dom = select(ts_dynamic_fixed_dom, [:EBUS, :BUS, :FOREST_OF_TREES, :PROPOSAL_ARRAY])
 rename_map = Dict(:EBUS => :EBUS, :FOREST_OF_TREES => :FT, :PROPOSAL_ARRAY => :DPA, :BUS => :BUS)
 rename!(ts_dynamic_fixed_dom, rename_map)
 
@@ -19,19 +19,22 @@ ts_dynamic_var_dom = select(ts_dynamic_var_dom, [:EBUS, :BUS, :PROPOSAL_ARRAY])
 rename_map = Dict(:EBUS => :EBUS, :PROPOSAL_ARRAY => :DPA, :BUS => :BUS)
 rename!(ts_dynamic_var_dom, rename_map)
 
+markers = [:circle, :rect, :utriangle, :xcross]
+colors = Dict("EBUS" => 1, "FT" => 2, "DPA" => 3, "BUS" => 4)
+
 Ns = [10^i for i in 3:7]
 
 function plot_vals(Ns, ts, title, xlabel, ylabel, pname)
     p = nothing
     for (i, k) in enumerate(names(ts))
         if i == 1
-            p = plot(Ns, ts[!, k], xscale=:log10, marker=:circle, xticks=Ns, 
-                 xlabel=xlabel, ylabel=ylabel,
+            p = plot(Ns, ts[!, k], xscale=:log10, marker=markers[i], xticks=Ns, 
+                 xlabel=xlabel, ylabel=ylabel, markersize=6, line = (2, :dash),
                  title=title, label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
-                 ylims=(0, Inf), widen = true, legend=:topleft, right_margin=10Plots.mm)
+                 ylims=(0, Inf), widen = true, legend=:topleft, right_margin=10Plots.mm, color = colors[k])
         else
-            plot!(Ns, ts[!, k], marker=:circle, label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
-                ylims=(0, Inf), widen = true)
+            plot!(Ns, ts[!, k], marker=markers[i], label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
+                ylims=(0, Inf), widen = true, color = colors[k], markersize=6, line = (2, :dash),)
         end
     end
     savefig(p, "figures/" * pname * ".pdf")
