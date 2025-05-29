0e035969df9aa0681e34f257e6fcaee61c49b723
tortar <adrianomeligrana1999@gmail.com>
2025-05-30 00:28:35 +0200

    improve plots

--- plot.jl
+++ plot.jl
@@ -19,7 +19,7 @@ ts_dynamic_var_dom = select(ts_dynamic_var_dom, [:EBUS, :BUS, :PROPOSAL_ARRAY])
 rename_map = Dict(:EBUS => :EBUS, :PROPOSAL_ARRAY => :DPA, :BUS => :BUS)
 rename!(ts_dynamic_var_dom, rename_map)
 
-markers = [:circle, :rect, :utriangle, :xcross]
+markers = Dict("EBUS" => :circle, "FT" => :rect, "DPA" => :utriangle, "BUS" => :xcross)
 colors = Dict("EBUS" => 1, "FT" => 2, "DPA" => 3, "BUS" => 4)
 
 Ns = [10^i for i in 3:7]
@@ -28,12 +28,12 @@ function plot_vals(Ns, ts, title, xlabel, ylabel, pname)
     p = nothing
     for (i, k) in enumerate(names(ts))
         if i == 1
-            p = plot(Ns, ts[!, k], xscale=:log10, marker=markers[i], xticks=Ns, 
+            p = plot(Ns, ts[!, k], xscale=:log10, marker=markers[k], xticks=Ns, 
                  xlabel=xlabel, ylabel=ylabel, markersize=6, line = (2, :dash),
                  title=title, label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
                  ylims=(0, Inf), widen = true, legend=:topleft, right_margin=10Plots.mm, color = colors[k], dpi=1000)
         else
-            plot!(Ns, ts[!, k], marker=markers[i], label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
+            plot!(Ns, ts[!, k], marker=markers[k], label=(k == "DPA" ? (string(k) * "*") : (k == "BUS_jl" ? "BUS-OPT" : string(k))),
                 ylims=(0, Inf), widen = true, color = colors[k], markersize=6, line = (2, :dash), dpi=1000)
         end
     end
