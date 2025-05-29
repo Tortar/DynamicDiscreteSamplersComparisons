b0ea885940e82c98ae9001652514034efcbd7bf5
tortar <adrianomeligrana1999@gmail.com>
2025-05-29 21:00:54 +0200

    new plots

--- compare.jl
+++ compare.jl
@@ -18,15 +18,15 @@ ts_static = Dict(
 )
 for N in Ns
 	w = initialize_weights_EBUS(rng, FixedSizeWeights, N)
-	t_static_EBUS = 10^9 * median_time(@be static_samples_EBUS($rng, $w, $N)) / N
+	t_static_EBUS = 10^9 * median_time(@be static_samples_EBUS($rng, $w, $N) seconds=20) / N
 	push!(ts_static["EBUS"], t_static_EBUS)
 
     ds = initialize_sampler_BUS_opt(rng, N)
-    t_static_BUS_opt = 10^9 * median_time(@be static_samples_BUS_opt($rng, $ds, $N)) / N
+    t_static_BUS_opt = 10^9 * median_time(@be static_samples_BUS_opt($rng, $ds, $N) seconds=20) / N
     push!(ts_static["BUS_jl"], t_static_BUS_opt)
 
     al = initialize_ALIAS_TABLE(rng, N)
-    t_static_AL = 10^9 * median_time(@be static_samples_ALIAS_TABLE($rng, $al, $N)) / N
+    t_static_AL = 10^9 * median_time(@be static_samples_ALIAS_TABLE($rng, $al, $N) seconds=20) / N
     push!(ts_static["ALIAS_TABLE"], t_static_AL)
 end
 df = DataFrame(ts_static)
@@ -39,11 +39,11 @@ ts_dynamic_fixed_dom = Dict(
 )
 for N in Ns
 	w = initialize_weights_EBUS(rng, FixedSizeWeights, N)
-	t_dynamic_fixed_dom_EBUS = 10^9 * median_time(@be dynamic_samples_fixed_dom_EBUS($rng, $w, $N)) / N
+	t_dynamic_fixed_dom_EBUS = 10^9 * median_time(@be dynamic_samples_fixed_dom_EBUS($rng, $w, $N) seconds=20) / N
 	push!(ts_dynamic_fixed_dom["EBUS"], t_dynamic_fixed_dom_EBUS)
 
     ds = initialize_sampler_BUS_opt(rng, N)
-    t_dynamic_fixed_dom_BUS_opt = 10^9 * median_time(@be dynamic_samples_fixed_dom_BUS_opt($rng, $ds, $N)) / N
+    t_dynamic_fixed_dom_BUS_opt = 10^9 * median_time(@be dynamic_samples_fixed_dom_BUS_opt($rng, $ds, $N) seconds=20) / N
     push!(ts_dynamic_fixed_dom["BUS_jl"], t_dynamic_fixed_dom_BUS_opt)
 end
 df = DataFrame(ts_dynamic_fixed_dom)
@@ -55,12 +55,12 @@ ts_dynamic_var_dom = Dict(
   "BUS_jl" => Float64[]
 )
 for N in Ns
-	t_dynamic_var_dom_EBUS = @be initialize_weights_EBUS(rng, ResizableWeights, N) dynamic_samples_variable_dom_EBUS($rng, _, $N) evals=1
+	t_dynamic_var_dom_EBUS = @be initialize_weights_EBUS(rng, ResizableWeights, N) dynamic_samples_variable_dom_EBUS($rng, _, $N) evals=1 seconds=20
 	t_dynamic_var_dom_EBUS = median_time(t_dynamic_var_dom_EBUS)
 	t_dynamic_var_dom_EBUS *= 10^9 / (9*N)
 	push!(ts_dynamic_var_dom["EBUS"], t_dynamic_var_dom_EBUS)
 
-    t_dynamic_var_dom_BUS_opt = @be initialize_sampler_BUS_opt(rng, N) dynamic_samples_variable_dom_BUS_opt($rng, _, $N) evals=1
+    t_dynamic_var_dom_BUS_opt = @be initialize_sampler_BUS_opt(rng, N) dynamic_samples_variable_dom_BUS_opt($rng, _, $N) evals=1 seconds=20
     t_dynamic_var_dom_BUS_opt = median_time(t_dynamic_var_dom_BUS_opt)
     t_dynamic_var_dom_BUS_opt *= 10^9 / (9*N)
     push!(ts_dynamic_var_dom["BUS_jl"], t_dynamic_var_dom_BUS_opt)
